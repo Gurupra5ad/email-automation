6a89cebaf5cded069783777edf74ff103103cc21
Gurupra5ad <guruprasad2511@gmail.com>
2021-03-02 18:18:07 +0530

    updated to send bulk emails

--- bulk_mail.py
+++ bulk_mail.py
@@ -1,9 +1,17 @@
 from openpyxl import load_workbook
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login('tedwaitforitmosby05@gmail.com', 'iloverobin')
+                      
 wb = load_workbook(filename='mail_id.xlsx', read_only=True)
 ws = wb['emails']
 
 for row in ws.rows:
     for cell in row:
-        print(cell.value)
-
+        server.sendmail('tedwaitforitmosby05@gmail.com',
+                cell.value,                      
+                 'Hey, This is smtp automated mail'
+                 )
 wb.close()
